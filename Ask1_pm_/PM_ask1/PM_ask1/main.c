24154ac3094414136f2c2bc1e6a57f137bf8a7cb
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-03-22 23:23:04 +0200

    ini

---
+++ Ask1_pm_/PM_ask1/PM_ask1/main.c
@@ -0,0 +1,30 @@
+/*
+ * PM_ask1.c
+ *
+ * Created: 22-Mar-23 10:04:49 PM
+ * Author : kzach
+ */ 
+/*
+#include <avr/io.h>
+
+
+int main(void)
+{
+	
+	
+    // Replace with your application code
+    while (1) 
+    {
+    }
+}
+*/
+#include <avr/io.h>
+#include <util/delay.h>
+/*#include "io.h"
+#include "delay.h"*/
+#define del 5
+int main(void){	//PIN is output	PORTD.DIR |= 0b00000010; //PIN1_bm
+	//LED is off	PORTD.OUT |= 0b00000010; //PIN1_bm
+	while (1) {
+		//on		PORTD.OUTCLR= 0b00000010; //PIN1_bm		_delay_ms(del); //wait for 10ms		//off		PORTD.OUT |= 0b00000010; //PIN1_bm		_delay_ms(del); //wait for 10ms
+	}}
