037fa91770424d34d891dff7b9cb848f16c940d4
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-03-28 21:30:37 +0300

    code from lab

--- Ask1_pm_/PM_ask1/PM_ask1/main.c
+++ Ask1_pm_/PM_ask1/PM_ask1/main.c
@@ -1,34 +1,89 @@
-#include <avr/io.h>#include <util/delay.h>#include <avr/interrupt.h>//#include <avr/iom4808.h>//#include "iom4808.h"
-#define ped 40
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+//#include <avr/iom4808.h>
+//#include "iom4808.h"
+
+#define ped 20
 #define del 10
 
-int x=0;
-int car=0;
+int time_3=0;
 int tram=0;
 int pezoi=0;
 
 //_delay_ms(del);
-int main(void) 
+int main(void)
 {
-			PORTD.DIR |= 0b00000111; //PINs are output			PORTD.OUT |= 0b00000111; //LEDs are off			PORTD.OUTCLR= 0b00000100; //Car LED is on			PORTF.PIN5CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;			//timerstuff:			//Tram:			TCA0.SPLIT.CTRLD = 1; //TCA_SPLIT_SPLITM_bm  0x01  /* Split Mode Enable bit mask. */			TCA0.SPLIT.LCNT = 0; //clear counter			TCA0.SPLIT.LCMP0= ped; //T1			TCA0.SPLIT.CTRLB=TCA_SPLIT_LCMP0EN_bm;			//Pezoi:			TCA0.SPLIT.HCNT = 0; //clear counter			TCA0.SPLIT.HCMP0= del; //T2,T3			TCA0.SPLIT.CTRLB=TCA_SPLIT_HCMP0EN_bm;			//Enable			TCA0.SPLIT.CTRLA = 0x7<<1; //TCA_SPLIT_CLKSEL_DIV1024_gc = (0x07<<1)			TCA0.SPLIT.CTRLA |=1;//Enable			TCA0.SPLIT.INTCTRL = TCA_SPLIT_LCMP0_bm; //TCA_SPLIT_LCMP0_bm  0x10 /* Low Compare 0 Interrupt Enable bit mask. */													 //TCA_SPLIT_LCMP0EN_bm  0x01  /* Low Compare 0 Enable bit mask. */													 //TCA_SPLIT_HCMP0EN_bm  0x10  /* High Compare 0 Enable bit mask. *///timerstuff://					-------- Tram -------- 	//while(1){tram=0}					sei(); //begin accepting interrupt signals			while (tram==0) 
-			{
-				//					-------- Pezoi --------				while (pezoi==0)				{					PORTD.OUTCLR= 0b00000100; //Car LED is on				}				PORTD.OUT |= 0b00000101; //off
-				PORTD.OUTCLR= 0b00000100; //Car LED is on
-			}			
-			cli();
-}
-		
-ISR(PORTF_PORT_vect)
-{
-	asm("break");
-	cli();
-	int y = PORTF.INTFLAGS;	PORTF.INTFLAGS=y;
+	PORTD.DIR |= 0b00000111; //PINs are output
 	PORTD.OUT |= 0b00000111; //LEDs are off
-	PORTD.OUTCLR= 0b00000001; //Pezoi LED is on
-	while(TCA0.SPLIT.HCMP0)
-	{		
+	PORTD.OUTCLR= 0b00000100; //Car LED is on
+	sei(); //begin accepting interrupt signals
+	PORTF.PIN5CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
+
+
+	//timerstuff:
+	//Tram:
+	TCA0.SPLIT.CTRLD = 1; //TCA_SPLIT_SPLITM_bm  0x01  /* Split Mode Enable bit mask. */
+	TCA0.SPLIT.CTRLB = 0;
+	TCA0.SPLIT.LCNT = ped; //T1
+	TCA0.SPLIT.LCMP0 = 0;
+
+	//Pezoi:
+	TCA0.SPLIT.HCNT = del; //T2,T3
+	TCA0.SPLIT.HCMP0= 0;
+	//Enable
+	TCA0.SPLIT.CTRLA = 0x7<<1; //TCA_SPLIT_CLKSEL_DIV1024_gc = (0x07<<1)
+	TCA0.SPLIT.CTRLA |=1;//Enable
+	TCA0.SPLIT.INTCTRL = TCA_SPLIT_LCMP0_bm; //TCA_SPLIT_LCMP0_bm  0x10 /* Low Compare 0 Interrupt Enable bit mask. */
+	TCA0.SPLIT.INTCTRL = TCA_SPLIT_HUNF_bm; //0x02    /* High Underflow Interrupt Enable bit mask. */
+	//TCA_SPLIT_LCMP0EN_bm  0x01  /* Low Compare 0 Enable bit mask. */
+	//TCA_SPLIT_HCMP0EN_bm  0x10  /* High Compare 0 Enable bit mask. */
+	//timerstuff:
+	//                    -------- Tram --------
+	//while(1){tram=0}
+
+	while (tram==0)
+	{
+		//                    -------- Pezoi --------
+		while (pezoi==0)
+		{
+			PORTD.OUTCLR= 0b00000100; //Car LED is on
+		}
+		PORTD.OUT |= 0b00000101; //off
+		PORTD.OUTCLR= 0b00000100; //Car LED is on
+		if(pezoi==1 &&s tram==0)
+		{
+			TCA0.SPLIT.HCNT = del; //T2,T3
+			TCA0.SPLIT.HCMP0= 0;
+		}
+		cli();
 	}
-	pezoi=1;	sei();}
-ISR(TCA0_LCMP0_vect) //{//	TCA0.SPLIT.CTRLA = 0; //Disable	//clear flag	asm("break");	cli();	int intflags = TCA0.SPLIT.INTFLAGS;	TCA0.SPLIT.INTFLAGS=intflags;	PORTD.OUT |= 0b00000111; //LEDs are off	PORTD.OUTCLR= 0b00000011; //Tram & Pezoi LEDs are on			while(TCA0.SPLIT.HCMP0)	{			}		tram=1;
-	sei();
-}
+
+	ISR(PORTF_PORT_vect)
+	{
+		int y = PORTF.INTFLAGS;
+		PORTF.INTFLAGS=y;
+		PORTD.OUT |= 0b00000111; //LEDs are off
+		PORTD.OUTCLR= 0b00000001; //Pezoi LED is on
+		pezoi=1;
+	}
+
+	ISR(TCA0_LCMP0_vect) //
+	{
+		//clear flag
+		int intflags = TCA0.SPLIT.INTFLAGS;
+		TCA0.SPLIT.INTFLAGS=intflags;
+		PORTD.OUT |= 0b00000111; //LEDs are off
+		PORTD.OUTCLR= 0b00000011; //Tram & Pezoi LEDs are on
+		tram=1;
+	}
+
+	ISR(TCA0_HUNF_vect) //
+	{
+		//clear flag
+		int intflags = TCA0.SPLIT.INTFLAGS;
+		TCA0.SPLIT.INTFLAGS=intflags;
+		PORTD.OUT |= 0b00000111; //LEDs are off
+		PORTD.OUTCLR= 0b00000011; //Tram & Pezoi LEDs are on
+		time_3=1;
+	}
