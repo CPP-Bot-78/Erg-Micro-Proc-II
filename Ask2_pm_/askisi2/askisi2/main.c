711add9885c1d5ea23e5c5c94973fe625c633c1c
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-04-24 22:29:19 +0300

    ask2 proj ini

---
+++ Ask2_pm_/askisi2/askisi2/main.c
@@ -0,0 +1,21 @@
+/*
+ * askisi2.c
+ *
+ * Created: 24-Apr-23 10:10:11 PM
+ * Author : kzach
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+
+int main(void)
+{
+    while (1) 
+    {
+      
+    }
+}
+
