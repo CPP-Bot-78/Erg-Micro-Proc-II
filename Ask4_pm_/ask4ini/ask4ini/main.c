9e6017f85919aa63d978ab8fa1aee55ad9cdcc86
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-05-22 23:27:40 +0300

    99pc done

--- Ask4_pm_/ask4ini/ask4ini/main.c
+++ Ask4_pm_/ask4ini/ask4ini/main.c
@@ -1,3 +1,5 @@
+
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
@@ -14,7 +16,7 @@ void alarm();
 int alarm_count = 0; //counts up to 3 tries, then alarm starts
 int alarm_mode = 0; //if 0 -> enabling, if 1 -> disabling // na dw pou prepei na allaksei
 int timer_mode = 0; //if 0 -> timer, if 1 -> pwm
-int alarm_brake = 0; // from adc, if 1 alarm will be ready to break
+int alarm_brake = 0; // from adc, if 1 alarm will be ready to break //einai o metrhthts ths adc, kseroume sthn isr tou tca an prokeitai gia ton synargemo
 int correct_pass = 0; //counts up to 4, then password is correct
 
 int main()
@@ -22,7 +24,8 @@ int main()
             //Ports #####
     PORTD.DIR |= 0b00000001;
 	PORTD.OUT |= PIN0_bm; //LED is off
-    PORTF.PIN5CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc
+    PORTF.PIN5CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
+	PORTF.PIN6CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
 			//TCA #####
 	TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;    //enable interrupt Overflow
 	TCA0.SINGLE.INTCTRL |= TCA_SINGLE_CMP0_bm;  //enable interrupt COMP0
@@ -39,7 +42,7 @@ int main()
 
 ISR(PORTF_PORT_vect)
 {
-	cli{};
+	cli();
 	if(alarm_mode==0) enabling();
 	else if(alarm_mode==1) disabling();
 	sei();
@@ -47,9 +50,9 @@ ISR(PORTF_PORT_vect)
 
 void enabling()
 {
-	if(correct_pass = 0 || correct_pass = 2) // correct pass =1,3 ->6 || 0,2 ->5
+	if(correct_pass == 0 || correct_pass == 2) // correct pass =1,3 ->6 || 0,2 ->5
 	{
-		if(pin5on && PORTF.INTFLAGS || pin56on && PORTF.INTFLAGS)
+		if( (pin5on && PORTF.INTFLAGS) || (pin56on && PORTF.INTFLAGS) )
 		{
 			correct_pass++;
 			int y = pin5on;
@@ -62,7 +65,7 @@ void enabling()
 			PORTF.INTFLAGS=y;
 		}
 	}
-	else if(correct_pass = 1 || correct_pass = 3)
+	else if(correct_pass == 1 || correct_pass == 3)
 	{
 		if(pin6on && PORTF.INTFLAGS)
 		{
@@ -77,13 +80,13 @@ void enabling()
 			PORTF.INTFLAGS=y;
 		}
 	}
-	else if(correct_pass = 4)
+	if(correct_pass == 4)
 	{
 		correct_pass =0;
 		TCA0.SINGLE.CTRLB = 0;//normal
 		TCA0.SINGLE.CTRLA = 0x7<<1; //TCA_SINGLE_CLKSEL_DIV1024_gc
 		TCA0.SINGLE.CNT = 0; //clear counter
-		TCA0.SINGLE.CMP0 = 20; //count up to 20 to get out
+		TCA0.SINGLE.CMP0 = 3; //count up to 20 to get out
 		TCA0.SINGLE.CTRLA |=1;//Enable
 		int y = PORTF.INTFLAGS;
 		PORTF.INTFLAGS=y;
@@ -93,9 +96,9 @@ void enabling()
 void disabling()
 {
 	if (alarm_count==3) alarm();
-	if(correct_pass = 0 || correct_pass = 2) // correct pass =1,3 ->6 || 0,2 ->5
+	if(correct_pass == 0 || correct_pass == 2) // correct pass =1,3 ->6 || 0,2 ->5
 	{
-		if(pin5on && PORTF.INTFLAGS || pin56on && PORTF.INTFLAGS)
+		if( (pin5on && PORTF.INTFLAGS) || (pin56on && PORTF.INTFLAGS) )
 		{
 			correct_pass++;
 			int y = pin5on;
@@ -107,9 +110,10 @@ void disabling()
 			correct_pass=0;
 			int y = PORTF.INTFLAGS;
 			PORTF.INTFLAGS=y;
+			disabling();
 		}
 	}
-	else if(correct_pass = 1 || correct_pass = 3)
+	else if(correct_pass == 1 || correct_pass == 3)
 	{
 		if(pin6on && PORTF.INTFLAGS)
 		{
@@ -123,9 +127,10 @@ void disabling()
 			correct_pass=0;
 			int y = PORTF.INTFLAGS;
 			PORTF.INTFLAGS=y;
+			disabling();
 		}
 	}
-	else if(correct_pass = 4)
+	if(correct_pass == 4)
 	{
 		alarm_count=0;
 		alarm_mode=0;
@@ -133,7 +138,7 @@ void disabling()
 		alarm_brake = 0;
 		correct_pass =0;
 		ADC0.CTRLA = 0; //disable adc
-		TCA0.CTRLA = 0; //disable tca
+		TCA0.SINGLE.CTRLA = 0; //disable tca
 		PORTD.OUT= PIN0_bm; // Led is off
 		int y = PORTF.INTFLAGS;
 		PORTF.INTFLAGS=y;
@@ -142,7 +147,7 @@ void disabling()
 
 ISR(TCA0_OVF_vect)
 {
-    cli{};
+    cli();
     int intflags =TCA0.SINGLE.INTFLAGS;
     TCA0.SINGLE.INTFLAGS = intflags;
 	PORTD.OUTCLR = PIN0_bm; //LED is on
@@ -151,9 +156,9 @@ ISR(TCA0_OVF_vect)
 
 ISR(TCA0_CMP0_vect)
 {
-    cli{};
-	if (alarm_brake==1) alarm();
-	if(timer_mode)//timer
+    cli();
+	if (alarm_brake==1) alarm();//einai o metrhthts ths adc
+	if(!timer_mode)//timer
 	{
 		ADC0.CTRLA |= ADC_RESSEL_10BIT_gc; //10-bit resolution
 		ADC0.CTRLA |= ADC_FREERUN_bm; //Free-Running mode enabled
@@ -170,26 +175,27 @@ ISR(TCA0_CMP0_vect)
     sei();
 }
 
-ISR(ADC0_WCOMP_vect)
+ISR(ADC0_WCOMP_vect)// na mhn metraei o metrhths synexeia
 {
-    cli{};
+    cli();
 	PORTD.OUTCLR= PIN0_bm; //LED 0 is on
 	alarm_mode=1;//to get in the right path from portf isr
-	alarm_brake=1; //ready to brake in tca cmp isr
+	alarm_brake=1; //ready to brake in tca cmp isr ,//einai o metrhthts ths adc
     TCA0.SINGLE.CTRLB = 0;//normal
     TCA0.SINGLE.CTRLA = 0x7<<1; //TCA_SINGLE_CLKSEL_DIV1024_gc
     TCA0.SINGLE.CNT = 0; //clear counter
-    TCA0.SINGLE.CMP0 = 30; //count up to 30 to start the alarm
+    TCA0.SINGLE.CMP0 = 3; //count up to 30 to start the alarm
     TCA0.SINGLE.CTRLA |=1;//Enable
-	disabling();
     int intflags = ADC0.INTFLAGS;
     ADC0.INTFLAGS = intflags;
     sei();
+	disabling();
 }
 
 void alarm()
 {
 	alarm_brake=0;//gia na mhn ksanaboume ston timer stin alarm
+	alarm_count++;//gia na mhn ksanabei se aythn thn synarthsh
 	timer_mode=1; //pwm mode in tca cmp isr
 	TCA0.SINGLE.CTRLA = 0;
 	TCA0.SINGLE.CNT=0;
