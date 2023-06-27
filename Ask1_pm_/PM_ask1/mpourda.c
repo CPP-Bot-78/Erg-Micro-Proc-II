fae03bcfd9c9b368e6a8bf0f3906e5c114b4ac84
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-06-27 22:20:15 +0300

    project ready as open source

--- Ask1_pm_/PM_ask1/mpourda.c
+++
@@ -1,5 +0,0 @@
-#include <avr/io.h>#include <util/delay.h>#include <avr/interrupt.h>#define ped 20
-int x=0;
-int main() {PORTD.DIR |= 0b00000010; //PIN is outputPORTD.OUTCLR= 0b00000010; //LED is on
-//(уел 219, 224, 205) 16-bit counter high and lowTCA0.SINGLE.CNT = 0; //clear counterTCA0.SINGLE.CTRLB = 0; //Normal Mode (TCA_SINGLE_WGMODE_NORMAL_gc уел 207)TCA0.SINGLE.CMP0 = ped; //When reaches this value -> interrupt CLOCK FREQUENCY/1024TCA0.SINGLE.CTRLA = 0x7<<1; //TCA_SINGLE_CLKSEL_DIV1024_gc уел 224TCA0.SINGLE.CTRLA |=1;//EnableTCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP0_bm; //Interrupt Enable (=0x10)sei(); //begin accepting interrupt signalswhile (x==0) {}PORTD.OUT |= PIN1_bm; //LED is offcli();}ISR(TCA0_CMP0_vect){
-	TCA0.SINGLE.CTRLA = 0; //Disable	//clear flag	int intflags = TCA0.SINGLE.INTFLAGS;	TCA0.SINGLE.INTFLAGS=intflags;	x=1;}
